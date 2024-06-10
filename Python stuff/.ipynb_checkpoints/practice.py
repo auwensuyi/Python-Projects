0889e17b6ac3eafaff87e0b97b4a870afaff4eb9
auwensuyi <auwensuyi1@outlook.com>
2024-06-09 23:24:58 -0500

    basic syantax #1

--- Python stuff/.ipynb_checkpoints/practice.py
+++ Python stuff/.ipynb_checkpoints/practice.py
@@ -1,3 +1,19 @@
 #Will be creating several notebooks
 print("Getting started")
 #placeholder
+x = 6
+y = 7
+z = x + y
+v = x - y
+t = x * y
+print(z)
+print(v)
+print(t)
+
+a = 100 
+b = 25
+
+c = a/b
+d = a%b
+print(c)
+print(d)
